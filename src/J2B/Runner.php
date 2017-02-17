7681ef4f43591af5cae8e3d26935d8efd4632c73
David Greminger <david.greminger@1up.io>
2017-02-17 14:58:17 +0100

    Add <head>-javascripts before <body>-javascripts

--- src/J2B/Runner.php
+++ src/J2B/Runner.php
@@ -11,10 +11,22 @@ class Runner
     public function moveJs(PageModel $objPage, LayoutModel $objLayout, PageRegular $objPageRegular)
     {
         if ('FE' === TL_MODE) {
+            if (!is_array($GLOBALS['TL_BODY'])) {
+                $GLOBALS['TL_BODY'] = [];
+            }
+
             foreach($GLOBALS['TL_JAVASCRIPT'] as $index => $javascript) {
                 $javascript = preg_replace('/\|.*/', '', $javascript);
 
-                $GLOBALS['TL_BODY'][] = '<script type="text/javascript" src="' . $javascript . '"></script>';
+                $GLOBALS['TL_BODY'] = array_merge(
+                    [
+                        sprintf(
+                            '<script type="text/javascript" src="%s"></script>',
+                            $javascript
+                        )
+                    ],
+                    $GLOBALS['TL_BODY']
+                );
 
                 unset($GLOBALS['TL_JAVASCRIPT'][$index]);
             }
